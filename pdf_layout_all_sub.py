4e0afa83339847dffefd08d92944c347f51b566e
songze <you@example.com>
2024-09-10 22:05:05 +0800

    update pdf_layout_all_sub

--- pdf_layout_all_sub.py
+++ pdf_layout_all_sub.py
@@ -1,4 +1,10 @@
 import os
+import sys
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+print("__dir__",  __dir__)
+sys.path.append(__dir__)
+# sys.path.insert(0, os.path.abspath(os.path.join(__dir__, "../")))
+print("sys.path", sys.path)
 import cv2
 import json
 import yaml
